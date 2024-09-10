706ee356e0bc97daef83facc7cc0897c0e6754e5
Adele Zilinskaite <zilinskaite.adele@gmail.com>
2024-09-10 22:51:35 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// function getComputerChoice() {
+//     let number = Math.random;
+//     if(number < 0.33){
+//         return 'Rock';
+//     } else if(number > 0.33 && number < 0.66){
+//         return 'Paper';
+//     } else {
+//         return 'Scissors';
+//     }
+// };
+
+// 1. function that randomly returns rock, paper or scissors
+
+
+// 2. funcition to get user's choice
