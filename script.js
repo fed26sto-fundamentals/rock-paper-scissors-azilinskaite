332c83619d9281acea6cc36e0966753228481d1d
Adele Zilinskaite <zilinskaite.adele@gmail.com>
2024-09-11 13:41:35 +0200

    Function is running

--- script.js
+++ script.js
@@ -1,15 +1,53 @@
-// function getComputerChoice() {
-//     let number = Math.random;
-//     if(number < 0.33){
-//         return 'Rock';
-//     } else if(number > 0.33 && number < 0.66){
-//         return 'Paper';
-//     } else {
-//         return 'Scissors';
-//     }
-// };
+let compChoice;
+let humanChoice;
+let scorePlayer = 0;
+let scoreComputer = 0;
 
-// 1. function that randomly returns rock, paper or scissors
+//get computer choice
+function getComputerChoice() {
+  const options = ["rock", "paper", "scissors"];
+  compChoice = options[Math.floor(Math.random() * options.length)];
+  return compChoice;
+}
 
+//get user choice
+function getHumanChoice() {
+  let answer = prompt("Rock, Paper or Scissors?");
+  humanChoice = answer.toLowerCase();
+  return humanChoice;
+}
 
-// 2. funcition to get user's choice
+//compare who wins
+function playRound() {
+  console.log(humanChoice);
+  console.log(compChoice);
+  if (humanChoice === compChoice) {
+    console.log("It's a draw.");
+  } else if (humanChoice === "rock" && compChoice === "scissors") {
+    console.log("Player wins!");
+    scorePlayer++;
+  } else if (humanChoice === "paper" && compChoice === "rock") {
+    console.log("Player wins!");
+    scorePlayer++;
+  } else if (humanChoice === "scissors" && compChoice === "paper") {
+    console.log("Player wins!");
+    scorePlayer++;
+  } else {
+    console.log("Computer wins!");
+    scoreComputer++;
+  }
+};
+
+function playGame() {
+    for (let step = 0; step < 5; step++){
+        getHumanChoice();
+        getComputerChoice();
+        playRound();
+        console.log("Player score: " + scorePlayer);
+        console.log("Computer score: " +scoreComputer);
+    }
+};
+
+playGame();
+
+//compare scores, declare winner
